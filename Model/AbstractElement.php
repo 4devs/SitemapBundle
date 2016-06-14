4e58b1fefa280438255448ddf2ca324e2eae8694
andrey1s <andrey@4devs.pro>
2016-06-14 15:33:21 +0300

    add PropertyAccessor

--- Model/AbstractElement.php
+++ Model/AbstractElement.php
@@ -2,12 +2,20 @@
 
 namespace FDevs\SitemapBundle\Model;
 
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+
 abstract class AbstractElement implements ExtendedElementInterface
 {
     /**
      * @var array
      */
     private $namespace = [];
+
+    /**
+     * @var PropertyAccessor
+     */
+    private $propertyAccessor;
 
     /**
      * @param \DOMDocument $dom
@@ -19,9 +27,8 @@ abstract class AbstractElement implements ExtendedElementInterface
         $element = $dom->createElement($this->getName());
 
         foreach ($this->getTags() as $tag) {
-            if ($val = $this->{'get'.ucfirst($tag)}()) {
-                $el = $dom->createElement($tag, $val);
-                $element->appendChild($el);
+            if ($val = $this->getTagValue($tag)) {
+                $this->appendChild($dom, $tag, $val, $element);
             }
         }
         $children = $this->getChildren();
@@ -30,6 +37,20 @@ abstract class AbstractElement implements ExtendedElementInterface
         }
 
         return $element;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return mixed
+     */
+    protected function getTagValue($name)
+    {
+        if (!$this->propertyAccessor) {
+            $this->propertyAccessor = PropertyAccess::createPropertyAccessor();
+        }
+
+        return $this->propertyAccessor->getValue($this, $name);
     }
 
     /**
@@ -72,9 +93,11 @@ abstract class AbstractElement implements ExtendedElementInterface
     }
 
     /**
-     * @param \DOMDocument              $dom
-     * @param \DOMElement               $element
+     * @param \DOMDocument             $dom
+     * @param \DOMElement              $element
      * @param ExtendedElementInterface $extendingElement
+     *
+     * @return \DOMElement
      */
     private function appendExtendingElement(\DOMDocument $dom, \DOMElement $element, ExtendedElementInterface $extendingElement)
     {
@@ -90,21 +113,35 @@ abstract class AbstractElement implements ExtendedElementInterface
             $this->appendExtendingElement($dom, $element, $child);
         }
         $this->namespace = array_merge($this->namespace, $extendingElement->getNamespace());
+
+        return $el;
     }
 
     /**
-     * @param \DOMDocument $dom
-     * @param \DOMElement  $element
-     * @param string       $name
-     * @param string|null  $val
+     * @param \DOMDocument      $dom
+     * @param \DOMElement       $element
+     * @param string            $name
+     * @param string|null|array $val
      *
      * @return \DOMElement
      */
-    private function appendChild(\DOMDocument $dom, \DOMElement $element, $name, $val)
+    private function appendChild(\DOMDocument $dom, $name, $val, \DOMElement $element = null)
     {
-        $el = $dom->createElement($name, $val);
-        $element->appendChild($el);
+        $element = $element ?: $dom->createElement($name);
+        $el = null;
+        if (is_array($val)) {
+            foreach ($val as $name => $item) {
+                if ($item instanceof ExtendedElementInterface) {
+                    $el = $this->appendExtendingElement($dom, $element, $item);
+                } else {
+                    $el = $this->appendChild($dom, $name, $item, $element);
+                }
+            }
+        } else {
+            $el = $dom->createElement($name, $val);
+            $element->appendChild($el);
+        }
 
-        return $el;
+        return $el ?: $element;
     }
 }
