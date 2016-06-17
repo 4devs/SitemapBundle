7ba9e175eefd87efec4b27918e7e123c003a4e9d
andrey1s <andrey@4devs.pro>
2016-06-17 15:29:38 +0300

    fix path to route6 remove registred command

--- FDevsSitemapBundle.php
+++ FDevsSitemapBundle.php
@@ -7,6 +7,7 @@ use FDevs\Bridge\Sitemap\DependencyInjection\Compiler\FactoryPass;
 use FDevs\Bridge\Sitemap\DependencyInjection\FDevsSitemapExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Console\Application;
 
 class FDevsSitemapBundle extends Bundle
 {
@@ -28,5 +29,25 @@ class FDevsSitemapBundle extends Bundle
     protected function createContainerExtension()
     {
         return new FDevsSitemapExtension();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPath()
+    {
+        if (null === $this->path) {
+            $reflected = new \ReflectionClass('FDevs\Bridge\Sitemap\SitemapEvents');
+            $this->path = dirname($reflected->getFileName());
+        }
+
+        return $this->path;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerCommands(Application $application)
+    {
     }
 }
